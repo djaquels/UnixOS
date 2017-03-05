35b0820c780dcf8b74e7191ab6d89ffb6ab07700
heptorsj <hjacales@gmail.com>
2017-03-04 21:46:35 -0600

    Comments

--- BinaryTree.c
+++ BinaryTree.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// The next program represents a binary tree ejecution process
+// For Unix like systems (FreeBSD,OpenBSD,Linux)
 main(){
  int i,j,e,f;
- for(j=0;j < 3; j++){
-  if(fork() == 0){
+ int prof = 3 // The depth of the tree - 1
+ for(j=0;j < prof ; j++){
+  if(fork() == 0){ // The son prints info
     printf("%d => %d\n",getppid(),getpid());
   }
-  else{
+  else{ // Father waits son to end and then
    wait(&e);
-   printf("%d ha despertado\n",getpid());
-   if( fork() == 0){
-     printf("%d => %d\n",getppid(),getpid());
+   printf("%d ha despertado\n",getpid()); // Wakes up
+   if( fork() == 0){ // Creates the second branch (son)
+     printf("%d => %d\n",getppid(),getpid()); // The son prints info
    }else{
-      wait(&f); 
-      printf("%d ha muerto :(\n",getpid());
+      wait(&f); // The father waits and ...
+      printf("%d ha muerto :(\n",getpid()); // Second time wakes up
+      // The father deads
       exit(1);
-    }
+    } // This process repeats till high rank father dead.
   }
  }
 
